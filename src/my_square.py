51ddf32f627677fe8dcf98014ac6689085ef08ff
David Albone <dpauk@users.noreply.github.com>
2017-02-04 11:12:20 +0000

    Changed to use * operator and square 24

--- src/my_square.py
+++ src/my_square.py
@@ -1,7 +1,7 @@
-def my_square(x):
+def my_square(z):
     """takes a number and returns the square
-    uses the ** operator
+    uses the * operator
     """
-    return x ** 2
+    return z * z
 
-print(my_square(4))
+print(my_square(24))
